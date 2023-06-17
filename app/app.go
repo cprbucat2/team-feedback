df3df223b226df8fcaff45b18c80b024383a36fc
Aiden Woodruff <aiden.woodruff@gmail.com>
2023-06-17 00:17:38 -0400

    Set trusted proxies

--- app/app.go
+++ app/app.go
@@ -12,6 +12,10 @@ func main() {
 	log.Print("Creating server.")
 
 	router := gin.Default()
+
+	router.ForwardedByClientIP = true
+	router.SetTrustedProxies([]string{"127.0.0.1"})
+
 	router.Static("/", "./www")
 	router.Run("0.0.0.0:8080")
 }
