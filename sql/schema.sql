7fa96824d9a16adbd3609b7efc2043218a0c398c
Aiden Woodruff <aiden.woodruff@gmail.com>
2023-07-16 18:27:52 -0400

    Update schema.sql
    Drop tables if they exist
    Change submissions to entries and membersubmissions to submissions
    Changed teams.TeamName to teams.Name
    Changed members to users
    Changed to users.Name
    
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,26 +1,32 @@
-create table teams (
-  ID int auto_increment not null,
-	TeamName varchar(255) not null,
-  Members varchar(255) not null,
-  primary key (ID)
-);
+drop table if exists teams;
+drop table if exists members;
+drop table if exists membersubmissions;
+drop table if exists submissions;
 
-create table members (
+create table teams (
 	ID int auto_increment not null,
-	MemberName varchar(255) not null,
+	Name varchar(255) not null,
+	Members varchar(255) not null,
 	primary key (ID)
 );
 
-create table membersubmissions (
+create table users (
 	ID int auto_increment not null,
-	UUID binary(16) not null, -- A uuid_to_bin encoded uuid.
-	Author int not null,
-	Submissions varchar(255) not null, -- A list of submission IDs.
-	Improvement varchar(255) not null,
+	Name varchar(255) not null,
+	TeamID int not null,
 	primary key (ID)
 );
 
 create table submissions (
+	ID int auto_increment not null,
+	UUID binary(16) not null, -- A uuid_to_bin encoded uuid.
+	Author int not null,
+	Entries varchar(255) not null, -- A list of submission IDs.
+	Improvement varchar(255) not null,
+	primary key (ID)
+);
+
+create table memberentries (
 	ID int auto_increment not null,
 	Member int not null,
 	Participation float not null,
