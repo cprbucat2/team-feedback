ba266255f4a269b14081962da39f7d22f7712df6
Aiden Woodruff <aiden.woodruff@gmail.com>
2023-07-17 22:41:54 -0400

    Use foreign keys
    Much better than string lists of ids haha
    Mostly lowercase columns
    Fixed drop table lines.
    Add none team and nobody user

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,39 +1,46 @@
 drop table if exists teams;
-drop table if exists members;
-drop table if exists membersubmissions;
+drop table if exists users;
 drop table if exists submissions;
+drop table if exists memberentries;
 
 create table teams (
-	ID int auto_increment not null,
-	Name varchar(255) not null,
-	Members varchar(255) not null,
-	primary key (ID)
+	id bigint auto_increment not null primary key,
+	name varchar(255) not null
 );
+
+insert into teams (name) values ("none");
 
 create table users (
-	ID int auto_increment not null,
-	Name varchar(255) not null,
-	TeamID int not null,
-	primary key (ID)
+	id bigint auto_increment not null primary key,
+	name varchar(255) not null,
+	team_id bigint not null,
+	index (team_id),
+	foreign key (team_id) references teams (id)
 );
+
+insert into users (name, team_id) values ("nobody", 1);
 
 create table submissions (
-	ID int auto_increment not null,
+	id bigint auto_increment not null primary key,
 	UUID binary(16) not null, -- A uuid_to_bin encoded uuid.
-	Author int not null,
-	Entries varchar(255) not null, -- A list of submission IDs.
-	Improvement varchar(255) not null,
-	primary key (ID)
+	author bigint not null,
+	improvement varchar(255) not null,
+	index (author),
+	foreign key (author) references users (id)
 );
 
-create table memberentries (
-	ID int auto_increment not null,
-	Member int not null,
+create table entries (
+	id bigint auto_increment not null primary key,
+	submission_id bigint not null,
+	member bigint not null,
 	Participation float not null,
 	Collaboration float not null,
 	Contribution float not null,
 	Attitude float not null,
 	Goals float not null,
 	Comment varchar(255),
-	primary key(ID)
+	index (submission_id),
+	foreign key (submission_id) references submissions (id),
+	index (member),
+	foreign key (member) references users (id)
 );
