55229ae7a662ac157108bcd002ae82237ac487e0
Aiden Woodruff <aiden.woodruff@gmail.com>
2023-07-21 15:58:44 -0400

    Adds admin template files

---
+++ app/www/public/js/useradmin.js
@@ -0,0 +1,38 @@
+/* eslint-env browser, jquery */
+jQuery(function ($) {
+	/**
+	 * Listen to checkbox clicks and update remove button and head checkbox.
+	 * @listens MouseEvent
+	 */
+	$(".user-list__checkbox").on("click", () => {
+		const boxes = $(".user-list__checkbox");
+		const count = boxes.map((i,e) => e.checked).get().reduce((x,sum) => x + sum);
+		if (count === 0) {
+			$(".remove-users-btn").prop("disabled", true);
+			$(".user-list__head-checkbox").prop("checked", false);
+			$(".user-list__head-checkbox").prop("indeterminate", false);
+		} else {
+			$(".remove-users-btn").prop("disabled", false);
+			$(".user-list__head-checkbox").prop("checked", count === boxes.length);
+			$(".user-list__head-checkbox").prop("indeterminate", count !== boxes.length);
+		}
+		$(".remove-users-btn").prop("value", `Remove (${count} selected)`);
+	});
+
+	/**
+	 * Update checkboxes based off the header checkbox.
+	 * @listens MouseEvent
+	 */
+	$(".user-list__head-checkbox").on("click", event => {
+		if (!event.target.checked) {
+			$(".user-list__checkbox").prop("checked", false);
+			$(".remove-users-btn").prop("disabled", true);
+			$(".remove-users-btn").prop("value", `Remove (0 selected)`);
+		} else {
+			const boxes = $(".user-list__checkbox");
+			boxes.prop("checked", true);
+			$(".remove-users-btn").prop("disabled", false);
+			$(".remove-users-btn").prop("value", `Remove (${boxes.length} selected)`);
+		}
+	});
+});
