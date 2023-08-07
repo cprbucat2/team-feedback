3d4a1ebd0ab19e60b781c80887602a077090a335
Aiden Woodruff <aiden.woodruff@gmail.com>
2023-08-06 23:28:39 -0400

    Rename .user-list to .admin-list and add nav links
    Handle .admin-list checkbox stuff in useradmin.js
    - We can reuse this code for team pages.
    Rename stylistic classes and retain utility classes
    - Some classes do matter for users specifically

--- app/www/public/js/useradmin.js
+++ app/www/public/js/useradmin.js
@@ -10,35 +10,35 @@ jQuery(function ($) {
 	 * Listen to checkbox clicks and update remove button and head checkbox.
 	 * @listens MouseEvent
 	 */
-	$(".user-list__checkbox").on("click", () => {
-		const boxes = $(".user-list__checkbox");
+	$(".admin-list__checkbox").on("click", () => {
+		const boxes = $(".admin-list__checkbox");
 		const count = boxes.map((i, e) => e.checked).get().reduce((x, sum) => x + sum);
 		if (count === 0) {
-			$(".remove-users-btn").prop("disabled", true);
-			$(".user-list__head-checkbox").prop("checked", false);
-			$(".user-list__head-checkbox").prop("indeterminate", false);
+			$(".admin-remove-btn").prop("disabled", true);
+			$(".admin-list__head-checkbox").prop("checked", false);
+			$(".admin-list__head-checkbox").prop("indeterminate", false);
 		} else {
-			$(".remove-users-btn").prop("disabled", false);
-			$(".user-list__head-checkbox").prop("checked", count === boxes.length);
-			$(".user-list__head-checkbox").prop("indeterminate", count !== boxes.length);
+			$(".admin-remove-btn").prop("disabled", false);
+			$(".admin-list__head-checkbox").prop("checked", count === boxes.length);
+			$(".admin-list__head-checkbox").prop("indeterminate", count !== boxes.length);
 		}
-		$(".remove-users-btn").prop("value", `Remove (${count} selected)`);
+		$(".admin-remove-btn").prop("value", `Remove (${count} selected)`);
 	});
 
 	/**
 	 * Update checkboxes based off the header checkbox.
 	 * @listens MouseEvent
 	 */
-	$(".user-list__head-checkbox").on("click", event => {
+	$(".admin-list__head-checkbox").on("click", event => {
 		if (!event.target.checked) {
-			$(".user-list__checkbox").prop("checked", false);
-			$(".remove-users-btn").prop("disabled", true);
-			$(".remove-users-btn").prop("value", `Remove (0 selected)`);
+			$(".admin-list__checkbox").prop("checked", false);
+			$(".admin-remove-btn").prop("disabled", true);
+			$(".admin-remove-btn").prop("value", `Remove (0 selected)`);
 		} else {
-			const boxes = $(".user-list__checkbox");
+			const boxes = $(".admin-list__checkbox");
 			boxes.prop("checked", true);
-			$(".remove-users-btn").prop("disabled", false);
-			$(".remove-users-btn").prop("value", `Remove (${boxes.length} selected)`);
+			$(".admin-remove-btn").prop("disabled", false);
+			$(".admin-remove-btn").prop("value", `Remove (${boxes.length} selected)`);
 		}
 	});
 });
